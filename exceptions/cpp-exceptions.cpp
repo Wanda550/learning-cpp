c5d0dd49d76bb459c81108c4c6c54f6755c0ed5f
slumlord <limelawanda6@gmail.com>
2026-02-01 15:04:31 +0200

    sixteeth commit: exception handling

--- exceptions/cpp-exceptions.cpp
+++ exceptions/cpp-exceptions.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdexcept>
 
 /*
     An exception is an error/problem that arises during the execution of a program,
@@ -8,23 +9,46 @@
     so that our program can continue running even after encountering an issue.
 
     try: run code inside this block in case of errors
-    throw: throws and exception when a problem is detected
+    throw: throws an exception when a problem is detected
     catch: catch an exception thrown try block and run code inside this block
 */
 
-void setRoomSlot(std::vector<int> roomSlots, int numPeople, int index) {
-    if (index >= roomSlots.size() || index < 0) {
-        throw -1;
-    } 
+int getRoomSlot(const std::vector<int>& roomSlots, int index) {
+
+    try {
+        int numPeople = roomSlots.at(index);
+        std::cout << "Room " << index << " has " << numPeople << " people\n";
+        return numPeople;
+    } catch (...) {
+        std::cerr << "getRoomSlot - Invalid room index: " << index << std::endl;
+        throw; // rethrow so caller can handle it too
+    }
+};
+
+void setRoomSlot(std::vector<int>& roomSlots, int index, int numPeople) {
+
+    int current = getRoomSlot(roomSlots, index);
+
+    if (current != 0) {
+        throw std::runtime_error("setRoomSlot - Room is already booked");
+    }
+
     roomSlots[index] = numPeople;
-}
+};
 
 int main() {
-    std::vector<int> roomSlots = {0, 0, 0, 0, 0};
-    setRoomSlot(roomSlots, 20, 15);
+    std::vector<int> roomSlots = {0, 20, 25, 0, 0};
 
-    std::cout << roomSlots[100] << std::endl;
+    try {
+        setRoomSlot(roomSlots, 20, 15); // invalid index or valid 
+    } catch(const std::out_of_range& e) { // error, e
+        std::cerr << "main - Out of range" << std::endl;
+    } catch(const std::exception& e) { // error, e
+        std::cerr << e.what() << std::endl;
+    }
+
+    // std::cout << roomSlots[100] << std::endl;
 
     std::cout << "DONE!" << std::endl;
 
-}
+};
